0250934e7aac80d069c22f78ea88312d3b1d57f1
Sara Cruz <sara.cruz@secret-source.eu>
2020-05-08 15:08:44 +0100

    creating new user and task - testing Postman

---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is envalid");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value.length < 6 || value.toLowerCase().includes("password")) {
+        throw new Error("password not valid");
+      }
+    },
+  },
+});
+
+module.exports = User;
