9382f5a0ae3f8cf45c2f9e57156205501ac239f3
Sara Cruz <sara.cruz@secret-source.eu>
2020-05-12 08:47:03 +0100

    routes finished

--- src/index.js
+++ src/index.js
@@ -2,82 +2,17 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000 
 
+
 app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
 
-
-
-
-// Users endpoints
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch(e){
-        res.status(400).send(e)
-    }
-})    
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    }catch(e){
-        res.status(400).send(e)
-    }
-
-})
-
-app.get('/users/:id', async (req, res) => {
-   const _id = req.params.id
-   try{
-      const user = await User.findById(_id)
-      if(!user){
-          return res.status(400).send()
-      } 
-      res.send(user)
-   }catch(e){
-      res.status(500).send(e)
-   }
-   
-})
-
-// Tasks Endpoints
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-    try{
-       await task.save()
-       res.status(201).send(task)
-    }catch(e){
-        res.status(404).send(e)
-    }
-})
-
-app.get('/tasks', async (req, res) => {
-  try{
-    const tasks = await Task.find({})
-    res.send(tasks)
-  }catch(e){
-    res.status(404).send(e)
-  }
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-    try{
-      const task = await Task.findById(_id)
-      res.send(task)
-    }catch(e){
-      res.status(500).send(e)  
-    }
-   
-})
 
 
 app.listen(port, () => {
